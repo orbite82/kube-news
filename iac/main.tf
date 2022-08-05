9237cf6af68c73ec2aefcc07400d9cbf353474d2
thiago-orbite <thiago.orbite@cliquefarma.com.br>
2022-08-05 03:53:26 -0300

    todos ajustes até aula 3

---
+++ iac/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_kubernetes_cluster" "orbite_k8s" {
+  name   = var.k8s_name
+  region = var.region
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.23.9-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-4vcpu-8gb"
+    node_count = 3
+
+  }
+}
+
+#resource "digitalocean_kubernetes_node_pool" "bar" {
+#  cluster_id = digitalocean_kubernetes_cluster.orbite_k8s.id
+#  name       = "premium"
+#  size       = "s-4vcpu-8gb"
+#  node_count = 1
+#
+#}
+
+variable "do_token" {}
+variable "k8s_name" {}
+variable "region" {}
+
+output "kube_endpoint" {
+  value = digitalocean_kubernetes_cluster.orbite_k8s.endpoint
+}
+
+resource "local_file" "kube_config" {
+    content  = digitalocean_kubernetes_cluster.orbite_k8s.kube_config.0.raw_config
+    filename = "kube_config.yaml"
+}
